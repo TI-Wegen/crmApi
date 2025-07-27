45e55bc05e19bc27bfd71260342a693b4e215af4
luis <luiszkm@gmail.com>
2025-07-27 12:49:21 -0300

    up

--- src/Api/CRM.API/Program.cs
+++ src/Api/CRM.API/Program.cs
@@ -3,6 +3,8 @@ using CRM.API.Configurations;
 using CRM.API.Hubs;
 using CRM.Infrastructure.Config.Meta;
 using Hangfire;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -49,8 +51,17 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/openapi/v1.json", "CRM API");
     });
 }
-//app.UseHealthChecks("/health");
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
+app.UseHealthChecksUI(options =>
+{
+    options.UIPath = "/monitor";
+    options.ApiPath = "/monitor-api";
+});
 app.UseHangfireDashboard("/hangfire");
 
 app.UseAuthentication();
--- src/Infrastructure/CRM.Infrastructure/Storage/S3Settings.cs
+++ src/Infrastructure/CRM.Infrastructure/Storage/S3Settings.cs
@@ -5,5 +5,5 @@ public class S3Settings
     public string BucketName { get; set; }
     public string AccessKey { get; set; } // Renomeado de Key para mais clareza
     public string SecretKey { get; set; }
-    public string Region { get; set; } // Adicionado para flexibilidade
+    public string Region { get; set; } 
 }
