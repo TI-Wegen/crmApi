790c22d97f8ead6cedcd7c6124eacaf9e26bfc2f
lucas <dev2@wegen.com.br>
2025-09-17 08:13:46 -0300

    up migrations

---
+++ src/Infrastructure/CRM.Infrastructure/Migrations/20250916134020_Add reaction field in message.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addreactionfieldinmessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReacaoMensagem",
+                table: "Mensagens",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReacaoMensagem",
+                table: "Mensagens");
+        }
+    }
+}
