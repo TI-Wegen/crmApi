1eae56bde346949958c122bba8192449310535ae
Ti Wegen <ti@wegen.com.br>
2025-10-07 09:44:31 -0300

    update database

---
+++ src/Infrastructure/CRM.Infrastructure/Migrations/20251007124252_AddIsAdminToAgente.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsAdminToAgente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Agentes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Agentes");
+        }
+    }
+}
