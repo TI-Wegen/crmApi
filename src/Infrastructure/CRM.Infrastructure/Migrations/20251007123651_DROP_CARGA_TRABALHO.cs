460c77a40615c9680faab3478b034b984a714b8e
Ti Wegen <ti@wegen.com.br>
2025-10-07 09:41:58 -0300

    update datatabse ef

---
+++ src/Infrastructure/CRM.Infrastructure/Migrations/20251007123651_DROP_CARGA_TRABALHO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DROP_CARGA_TRABALHO : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Agentes",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DropColumn(
+                name: "CargaDeTrabalho",
+                table: "Agentes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CargaDeTrabalho",
+                table: "Agentes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "Agentes",
+                columns: new[] { "Id", "CargaDeTrabalho" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), 0 });
+        }
+    }
+}
